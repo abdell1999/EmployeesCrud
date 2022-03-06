af83f587beee251fc9c3b3be1fc04406f162cc97
Abdellah <abdellahbukadduh99@gmail.com>
2022-03-06 14:02:10 +0100

    Metodo que trae un registro en función del id ya funciona correctamente!

--- backend/src/main/java/com/abdell/employees/controller/EmployeeController.java
+++ backend/src/main/java/com/abdell/employees/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +33,12 @@ public class EmployeeController {
         return new ResponseEntity(list, HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> get(@PathVariable("id") int id){
+
+        Employee Employee = employeeService.get(id).get();
+        return new ResponseEntity(Employee, HttpStatus.OK);
+    }
 
 
 
